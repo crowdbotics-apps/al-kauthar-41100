a309f45db7e6fc6457499b8602b439def61c1341
Crowdbotics <team@crowdbotics.com>
2023-05-30 14:58:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "al-kauthar-41100",
+  url: "https://al-kauthar-41100.botics.co",
+  api: "https://al-kauthar-41100.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
